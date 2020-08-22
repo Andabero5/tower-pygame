6faf399bab0e9a75361df697ef69541f8218577e
Andabero5 <adbeltranr@correo.udistrital.edu.co>
2020-08-21 22:07:16 -0500

    clone

--- logic/floor.py
+++ logic/floor.py
@@ -18,9 +18,13 @@ class Floor():
             self.speed = [0, 10]
         elif self.rect.right >= WIDTH or self.rect.left <= 0:
             self.speed[0] = -self.speed[0]
-        if self.rect.bottom >= HEIGHT-53:
+        if self.rect.bottom >= HEIGHT-51:
             self.speed[1] = 0
         self.rect.move_ip(self.speed)
 
     def clone(self):
         return deepcopy(self)
+
+
+floor = Floor
+copy(floor)
